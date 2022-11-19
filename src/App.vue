069c8cb1a9c948efeb4c668d354024e6953dc92d
mattialicausi <licausivitomattia@gmail.com>
2022-11-19 16:50:56 +0100

    preparazione repo

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center">
+    ciao
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+@import './assets/style/general.scss';
+
+
+</style>
